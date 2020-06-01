105e100273d4ab171f480b6aa2f6b49e0e75e8ec
Carl Sachs <carlton@Carls-MacBook-Air.local>
2020-06-01 17:53:56 -0400

    finished number 4

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,12 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print(str(x) + " " + str(y) + " " + z)
+
 # Use the 'format' string method to print the same thing
+string = "{} {} {}"
+print(string.format(x, y, z))
 
 # Finally, print the same thing using an f-string
+
+f"{x} {y} {z}"

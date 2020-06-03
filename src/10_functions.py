be8a3fa561d2b6d404a8913ea64c965bc580f883
Carl Sachs <carlton@Carls-MacBook-Air.local>
2020-06-02 20:27:10 -0400

    10 done

--- src/10_functions.py
+++ src/10_functions.py
@@ -3,10 +3,17 @@
 # YOUR CODE HERE
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+num = int(input("Enter a number: "))
+#num = int(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 
+def is_even(num):
+    if (num % 2) == 0:
+        print("{0} is an even number!".format((num)))
+    else:
+        print("{0} is an odd number!".format((num)))
+    
+is_even(num)
